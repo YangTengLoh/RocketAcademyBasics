c403a468d971d9c5674780b32cc37b5cae675513
YangTengLoh <101924938+YangTengLoh@users.noreply.github.com>
2022-04-02 16:14:05 +0800

    edited on 02 April 22

--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,35 +1,64 @@
-var playerOpponent = function () {
-  // Generate a decimal from 0 through 6, inclusive of 0 and exclusive of 6.
+var randomPlay = function () {
+  // Generate a decimal from 0 through 3, inclusive of 0 and exclusive of 3.
   var randomDecimal = Math.random() * 3;
   // Remove the decimal with the floor operation.
-  // This will be an integer from 0 to 5 inclusive.
+  // This will be an integer from 0 to 2 inclusive.
   var randomInteger = Math.floor(randomDecimal);
-  // Add 1 to get valid dice rolls of 1 through 6 inclusive.
-  var oppCode = randomInteger + 1;
-  var oppPlay = "Stone";
-  if (oppCode == 1) {
-    oppPlay = "Scissors";
-  } else if (oppCode == 2) {
-    oppPlay = "Paper";
-  }
-  return oppPlay;
+  return randomInteger;
 };
 
+// set global variables to record the number of wins and turns.
+var userWins = 0;
+var compWins = 0;
+var draws = 0;
+var turns = 0;
+var userName = "";
+// Store the options in array
+var gameList = ["Scissors", "Paper", "Stone"];
+//set var to store game status
+var gameStatus = "username";
 var main = function (input) {
-  var player2 = playerOpponent();
-  var message = `${input} versus ${player2}!`;
-  if (input != "Scissors" && input != "Paper" && input != "Stone") {
-    return "Error input! Please try again with Scissors, Paper or Stone~";
-  } else if (
-    (input == "Scissors" && player2 == "Paper") ||
-    (input == "Paper" && player2 == "Stone") ||
-    (input == "Stone" && player2 == "Scissors")
-  ) {
-    message += " You Win!";
-  } else if (input == player2) {
-    message += " It's a draw!";
-  } else {
-    message += " You Lose! Please try again~";
+  if (gameStatus == "username") {
+    userName = input;
+    gameStatus = "gamestart";
+    return `Hello ${userName} ! You may start the game.`;
+  } else if (gameStatus == "gamestart") {
+    // computer plays according to the random number
+    var compPlay = gameList[randomPlay()];
+    // activate AI if selected
+    if (input == "AI") {
+      input = gameList[randomPlay()];
+    }
+    // setup basic message to output
+    var message = `${input} versus ${compPlay}!`;
+    //validations -> check if the user wins
+    if (gameList.indexOf(input) == -1) {
+      return "Error input! Please try again with Scissors, Paper or Stone~";
+    } else {
+      //update the number of gameplays
+      turns += 1;
+      // winning condition -> output you win and update userWins
+      if (
+        (input == "Scissors" && compPlay == "Paper") ||
+        (input == "Paper" && compPlay == "Stone") ||
+        (input == "Stone" && compPlay == "Scissors")
+      ) {
+        message += " You Win!";
+        userWins += 1;
+      }
+      //draw -> output it's a draw and update draws
+      else if (input == compPlay) {
+        message += " It's a draw!";
+        draws += 1;
+      } else {
+        //other situation -> output you lose and update compWins
+        message += " You Lose! Please try again~";
+        compWins += 1;
+      }
+    }
+    // add winning records to the message above
+    var gamePlayer = ` Player: ${userName} `;
+    var gameStats = `Updated until Round ${turns}: ${userWins} vs ${compWins}, draws: ${draws}`;
+    return message + "<br>" + gamePlayer + "<br>" + gameStats;
   }
-  return message;
 };
